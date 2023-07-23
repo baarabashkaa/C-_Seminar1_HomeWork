3e37176e8385a59db260769108d06be5b5eec8b0
IlyaDubinin <ilya_k-somon96@mai.ru>
2023-07-23 17:53:18 +0800

    выполнил дз

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿// Домашнее заданее по 1 семинару
+
+
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("ведите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2= Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("Большее число: " + num1);
+// }
+
+// else
+// {
+//     Console.WriteLine("Большее число: " + num2);
+// }
+
+
+
+
+
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("ведите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num3= Convert.ToInt32(Console.ReadLine());
+
+//  if ((num1>num2) || (num1>num3))
+//  {
+//     Console.WriteLine("Большее число: " + num1);
+//  }
+ 
+//  else if (num2>num3) 
+//  {
+//     Console.WriteLine("Большее число: " + num2);
+//  }
+
+// else
+// {
+//     Console.WriteLine("Большее число: " + num3);
+// }
+
+
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("ведите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 % 2==0)
+// {
+//     Console.WriteLine("Да");
+// }
+
+// else
+// {
+//    Console.WriteLine("нет"); 
+// }
+
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("ведите число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// int numNeg= 2;
+
+// while(numNeg<=num1)
+// {
+//     Console.WriteLine(numNeg);
+//     numNeg=numNeg+2;
+// }
